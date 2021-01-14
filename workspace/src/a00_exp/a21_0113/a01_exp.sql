347aba1ead4980fd48fc0652cc3a2da8b4d47698
Taek <68735491+wangsungtaek@users.noreply.github.com>
2021-01-14 23:52:10 +0900

    210114

--- workspace/src/a00_exp/a21_0113/a01_exp.sql
+++ workspace/src/a00_exp/a21_0113/a01_exp.sql
@@ -15,7 +15,24 @@ SELECT * FROM TEST;
 	2) 기존 컬럼에 데이터가 있는 경우
 		- 타입변경 char, varchar2허용
 		- 변경한 컬럼의 크기가 저장된 데이터의 크기보다 같거나 클 경우만 가능
+		(1) key(primary key) empno와 함께 특정 임시컬럼으로 저장
+		(2) update 테이블명
+				set 컬럼명 = null;
+		(3) 컬럼 타입변경.
+		(4) 해당 타입에 맞는 유형을 to_xxx()함수를 이용해서 할당 후
+		(5) 필요없는 임시컬럼을 삭제 처리
+
+# 데이터가 있는 경우
+	1. 문자열(char, varchar2)
+		- 현재 입력된 데이터의 크기 범위 안에서 작거나 크게 변경이 가능하다.
+			입력된 데이터가 varchar2(10), length(컬럼명) 10이하일 때,
+			20로 변경하고 다시 11로 변경은 가능하다.
+			그러나, 10이하로 변경은 에러가 발생	
+	2. 숫자형
+		- 입력된 데이터 상관없이 정밀도를 한번 크게 변경이 되면,
+			다시 작게 변경이 불가능하다.
 **/
+
 --[중] 3. 테이블 구조복사 종합예제
 --    1) emp54으로 구조복사해서 테이블 만들고,
 --    2) 테이블을 dname, position 를 추가. 
@@ -42,12 +59,12 @@ INSERT INTO EMP54
 SELECT E.EMPNO, E.ENAME, E.JOB, E.MGR, E.HIREDATE, E.SAL, E.COMM,
 	   D.DEPTNO, D.DNAME, D.LOC,
 	   CASE
-	   	WHEN SAL>=5000 THEN 'A'
-	   	WHEN SAL>=4000 THEN 'B'
-	   	WHEN SAL>=3000 THEN 'C'
-	   	WHEN SAL>=2000 THEN 'D'
-	   	WHEN SAL>=1000 THEN 'E'
-	   	ELSE 'F'
+	   		WHEN SAL>=5000 THEN 'A'
+	   		WHEN SAL>=4000 THEN 'B'
+	   		WHEN SAL>=3000 THEN 'C'
+	   		WHEN SAL>=2000 THEN 'D'
+	   		WHEN SAL>=1000 THEN 'E'
+	   		ELSE 'F'
 	   END AS GRADE,
 	   TO_CHAR(HIREDATE, 'YYMM')||E.DEPTNO||E.EMPNO AS TEMP_EMPNO
   FROM EMP E, DEPT D, SALGRADE S
